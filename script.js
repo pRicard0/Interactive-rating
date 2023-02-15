c6429899613af0bb3354523a23685f9fd19824f9
pRicard0 <pauloricardo.jesus2011@gmail.com>
2023-02-14 23:32:42 -0300

    main part of javascript

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
+const mainDisplay = document.querySelector("#main");
+const form = document.querySelector(".form-container");
+const userSelection = document.querySelector("#selected-text");
+const thankYouDisplay = document.querySelector("#thankyou");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const rating = form.rating.value;
+  userSelection.textContent = `You selected ${rating} out of 5`;
+  mainDisplay.classList.remove("visible");
+  thankYouDisplay.classList.add("thx-visible");
+});
 
 
